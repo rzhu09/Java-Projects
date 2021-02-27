0c6d3b8524d72b7a611d2edcbbd5d63e7215f9a4
rzhu09 <60201899+rzhu09@users.noreply.github.com>
2021-02-26 20:56:21 -0500

    Update Core.java

--- Data_Base_1/src/Core.java
+++ Data_Base_1/src/Core.java
@@ -10,37 +10,35 @@ public class Core {
 		Teacher_Class syracuse = new Teacher_Class(3,"syracuse",600,3);
 		Teacher_Class nyc = new Teacher_Class(4,"nyc",600,4);
 
-        List<Teacher_Class> teacherList = new ArrayList<>();
-        teacherList.add(wada);
-        teacherList.add(kevin);
-        teacherList.add(syracuse);
-        teacherList.add(nyc);
+		List<Teacher_Class> teacherList = new ArrayList<>();
+		teacherList.add(wada);
+		teacherList.add(kevin);
+		teacherList.add(syracuse);
+		teacherList.add(nyc);
 
-        Student_Class ray = new Student_Class(1,"ray",4, 1000);
-        Student_Class kel = new Student_Class(2,"kel",12, 2000);
-        Student_Class sam = new Student_Class(3,"sam",5, 1034);
-        Student_Class shy = new Student_Class(4,"shy",5, 1003);
-        List<Student_Class> studentList = new ArrayList<>();
+		Student_Class ray = new Student_Class(1,"ray",4, 1000);
+		Student_Class kel = new Student_Class(2,"kel",12, 2000);
+		Student_Class sam = new Student_Class(3,"sam",5, 1034);
+		Student_Class shy = new Student_Class(4,"shy",5, 1003);
+		List<Student_Class> studentList = new ArrayList<>();
 
-        studentList.add(ray);
-        studentList.add(kel);
-        studentList.add(sam);
-        studentList.add(shy);
-        
-        School ms104 = new School(teacherList, studentList, 1000000, 200000);
-        
-        List<Teacher_Class> ms104_teachers = ms104.get_teachers();
-        
-        for(Teacher_Class t : ms104_teachers) {
-        	System.out.println(t.get_Name());
-        }
-        
-        List<Student_Class> ms104_students = ms104.get_students();
-        
-        for(Student_Class s : ms104_students) {
-        	System.out.println(s.get_name());
-        }
-        
-        
+		studentList.add(ray);
+		studentList.add(kel);
+		studentList.add(sam);
+		studentList.add(shy);
+
+		School ms104 = new School(teacherList, studentList, 1000000, 200000);
+
+		List<Teacher_Class> ms104_teachers = ms104.get_teachers();
+
+		for(Teacher_Class t : ms104_teachers) {
+			System.out.println(t.get_Name());
+		}
+
+		List<Student_Class> ms104_students = ms104.get_students();
+
+		for(Student_Class s : ms104_students) {
+			System.out.println(s.get_name());
+		}
 	}
 }
